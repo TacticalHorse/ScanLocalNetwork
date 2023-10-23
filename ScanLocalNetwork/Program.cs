46eaf738491b6c3887a83c5089eca7f3c1600c80
Михаил <onib2009@yandex.ru>
2023-10-23 17:40:26 +0500

    Добавьте файлы проекта.

---
+++ ScanLocalNetwork/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Net.NetworkInformation;
+
+namespace ScanLocalNetwork
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Получаем все сетевые интерфейсы на компьютере
+            NetworkInterface[] interfaces = NetworkInterface.GetAllNetworkInterfaces();
+
+            foreach (var networkInterface in interfaces)
+            {
+                // Проверяем, что интерфейс подключен и является локальной сетью
+                if (networkInterface.OperationalStatus == OperationalStatus.Up &&
+                    networkInterface.NetworkInterfaceType != NetworkInterfaceType.Loopback)
+                {
+                    Console.WriteLine("Имя интерфейса: " + networkInterface.Name);
+                    Console.WriteLine("MAC-адрес: " + networkInterface.GetPhysicalAddress().ToString());
+
+                    // Получаем IP-адреса интерфейса
+                    IPInterfaceProperties ipProps = networkInterface.GetIPProperties();
+                    foreach (var ipAddress in ipProps.UnicastAddresses)
+                    {
+                        // Проверяем, что это IPv4-адрес
+                        if (ipAddress.Address.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
+                        {
+                            Console.WriteLine("IP-адрес: " + ipAddress.Address.ToString());
+                            Console.WriteLine("Шлюз по умолчанию: " + ipProps.GatewayAddresses[0].Address.ToString());
+                            Console.WriteLine("Маска подсети: " + ipAddress.IPv4Mask.ToString());
+                        }
+                    }
+
+                    Console.WriteLine("=========================================");
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
