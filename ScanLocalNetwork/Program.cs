050ff0f8caeebddc174a24416a4afc6d848e49e1
Михаил <onib2009@yandex.ru>
2023-10-27 13:48:40 +0500

    Пробуем через арп

--- ScanLocalNetwork/Program.cs
+++ ScanLocalNetwork/Program.cs
@@ -7,11 +7,7 @@ namespace ScanLocalNetwork
 
         static async Task Main(string[] args)
         {
-            countdown = new CountdownEvent(1);
-            Stopwatch sw = new Stopwatch();
-            sw.Start();
-            string ipBase = "192.168.194.";
-            for (int i = 1; i < 255; i++)
+            Process process = new Process
             {
                 StartInfo = new ProcessStartInfo
                 {
@@ -22,63 +18,10 @@ namespace ScanLocalNetwork
                     UseShellExecute = false
                 }
             };
-
-        static void p_PingCompleted(object sender, PingCompletedEventArgs e)
-        {
-            string ip = (string)e.UserState;
-            if (e.Reply != null && e.Reply.Status == IPStatus.Success)
-            {
-                if (resolveNames)
-                {
-                    string name;
-                    try
-                    {
-                        IPHostEntry hostEntry = Dns.GetHostEntry(ip);
-                        name = hostEntry.HostName;
-                    }
-                    catch (SocketException ex)
-                    {
-                        name = "?";
-                    }
-                    Console.WriteLine("{0} ({1}) is up: ({2} ms)", ip, name, e.Reply.RoundtripTime);
-                }
-                else
-                {
-                    Console.WriteLine("{0} is up: ({1} ms)", ip, e.Reply.RoundtripTime);
-                }
-                lock (lockObj)
-                {
-                    upCount++;
-                }
-            }
-            else if (e.Reply == null)
-            {
-                Console.WriteLine("Pinging {0} failed. (Null Reply object?)", ip);
-            }
-            countdown.Signal();
-        }
-        public static string GetMacByIP(string ipAddress)
-        {
-            // grab all online interfaces
-            var query = NetworkInterface.GetAllNetworkInterfaces()
-                .Where(n =>
-                    n.OperationalStatus == OperationalStatus.Up && // only grabbing what's online
-                    n.NetworkInterfaceType != NetworkInterfaceType.Loopback)
-                .Select(_ => new
-                {
-                    PhysicalAddress = _.GetPhysicalAddress(),
-                    IPProperties = _.GetIPProperties(),
-                });
-
-            // grab the first interface that has a unicast address that matches your search string
-            var mac = query
-                .Where(q => q.IPProperties.UnicastAddresses
-                    .Any(ua => ua.Address.ToString() == ipAddress))
-                .FirstOrDefault()
-                .PhysicalAddress;
-
-            // return the mac address with formatting (eg "00-00-00-00-00-00")
-            return String.Join("-", mac.GetAddressBytes().Select(b => b.ToString("X2")));
+            process.Start();
+            await process.StandardInput.WriteLineAsync("arp -a");
+            var output = await process.StandardOutput.ReadLineAsync();
+            Console.WriteLine(output);
         }
     }
 }
