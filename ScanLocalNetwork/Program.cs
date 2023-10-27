6ef9cb49eef3e60e82673f01f9d041778f789a70
Михаил <onib2009@yandex.ru>
2023-10-27 13:45:57 +0500

    Пробуем через арп

--- ScanLocalNetwork/Program.cs
+++ ScanLocalNetwork/Program.cs
@@ -1,18 +1,11 @@
 ﻿using System.Diagnostics;
-using System.Net;
-using System.Net.NetworkInformation;
-using System.Net.Sockets;
 
 namespace ScanLocalNetwork
 {
     class Program
     {
-        static CountdownEvent countdown;
-        static int upCount = 0;
-        static object lockObj = new object();
-        const bool resolveNames = true;
 
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             countdown = new CountdownEvent(1);
             Stopwatch sw = new Stopwatch();
@@ -20,34 +13,19 @@ namespace ScanLocalNetwork
             string ipBase = "192.168.194.";
             for (int i = 1; i < 255; i++)
             {
-                string ip = ipBase + i.ToString();
-
-                Ping p = new Ping();
-                p.PingCompleted += new PingCompletedEventHandler(p_PingCompleted);
-                countdown.AddCount();
-                p.SendAsync(ip, 100, ip);
-            }
-            countdown.Signal();
-            countdown.Wait();
-            sw.Stop();
-            TimeSpan span = new TimeSpan(sw.ElapsedTicks);
-            Console.WriteLine("Took {0} milliseconds. {1} hosts active.", sw.ElapsedMilliseconds, upCount);
-            Console.ReadLine();
-        }
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = "bash",
+                    RedirectStandardInput = true,
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true,
+                    UseShellExecute = false
+                }
+            };
 
         static void p_PingCompleted(object sender, PingCompletedEventArgs e)
         {
             string ip = (string)e.UserState;
-            string mac = string.Empty;
-            try
-            {
-                mac = GetMacByIP(ip);
-            }
-            catch 
-            { 
-                mac = "?"; 
-            }
-
             if (e.Reply != null && e.Reply.Status == IPStatus.Success)
             {
                 if (resolveNames)
@@ -62,11 +40,11 @@ namespace ScanLocalNetwork
                     {
                         name = "?";
                     }
-                    Console.WriteLine("{0}[{3}] ({1}) is up: ({2} ms)", ip, name, e.Reply.RoundtripTime, mac);
+                    Console.WriteLine("{0} ({1}) is up: ({2} ms)", ip, name, e.Reply.RoundtripTime);
                 }
                 else
                 {
-                    Console.WriteLine("{0}[{2}] is up: ({1} ms)", ip, e.Reply.RoundtripTime, mac);
+                    Console.WriteLine("{0} is up: ({1} ms)", ip, e.Reply.RoundtripTime);
                 }
                 lock (lockObj)
                 {
